282cf98f9dcb64a6f2de5a935a896fff636a0981
IsmailHusun19 <ismailhusun2002>
2023-11-24 04:18:31 +0700

    update

---
+++ script/login.js
@@ -0,0 +1,19 @@
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.querySelector('form');
+
+//     form.addEventListener('submit', function (e) {
+//         e.preventDefault();
+
+//         const username = document.getElementById('username').value;
+//         const password = document.getElementById('password').value;
+
+
+//         if (username === 'user' && password === 'password') {
+//             alert('Login berhasil!');
+//             // Redirect atau tindakan lain setelah login berhasil
+//         } else {
+//             alert('Login gagal. Coba lagi.');
+//         }
+//     });
+// });
